c27a243ea8ddf0a0be0ff930b61aa1030fdfd53b
Paul Marechal <paulmarechal785@hotmail.fr>
2024-10-31 14:32:21 +0100

    Update img to look at camera

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,15 +1,15 @@
 import { Text } from '@react-three/drei';
-import { useFrame } from '@react-three/fiber';
+import { useFrame, useThree } from '@react-three/fiber'
 import { useRef } from 'react';
 import * as THREE from 'three';
 
 const imageUrls = [
     '/images/catacombes.webp',
     '/images/devxr_3D.webp',
-    '/images/paul_marechal.webp',
+    '/images/paul_marechal.webp'
 ];
 
-function RotatingImages({ images }) {
+function RotatingImages({ images, cameraPosition }) {
     const groupRef = useRef();
     const radius = 3; 
 
@@ -20,13 +20,21 @@ function RotatingImages({ images }) {
     return (
         <group ref={groupRef}>
             {images.map((url, index) => {
-                const angle = (index / images.length) * Math.PI * 2;
+                const angle = (index / images.length) * Math.PI * 1.5;
                 const x = radius * Math.cos(angle);
                 const z = radius * Math.sin(angle);
 
+                const imageRef = useRef();
+
+                useFrame(() => {
+                    if (imageRef.current) {
+                        imageRef.current.lookAt(new THREE.Vector3(...cameraPosition));
+                    }
+                });
+
                 return (
-                    <mesh position={[x, 0, z]} key={index}>
-                        <planeGeometry args={[1, 1.5]} />
+                    <mesh position={[x, 0, z]} ref={imageRef} key={index}>
+                        <planeGeometry args={[1.5, 1]} />
                         <meshBasicMaterial map={new THREE.TextureLoader().load(url)} />
                     </mesh>
                 );
@@ -36,9 +44,11 @@ function RotatingImages({ images }) {
 }
 
 export default function Experience() {
+    const { camera } = useThree();
+
     return (
         <>
-            <RotatingImages images={imageUrls} />
+            <RotatingImages images={imageUrls} cameraPosition={camera.position.toArray()} />
 
             <Text
                 position={[0, 0, 0]}
