da8e770d51727cc0e36aa3e942584a7cf2cc2d12
Paul Marechal <paulmarechal785@hotmail.fr>
2024-11-13 12:53:48 +0100

    Added redirection to mobile interface

--- src/index.jsx
+++ src/index.jsx
@@ -2,18 +2,34 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.jsx'
+import { BrowserView, MobileView } from 'react-device-detect';
+import { useEffect } from 'react';
+
+function MobileRedirect() {
+    useEffect(() => {
+        window.location.href = 'https://devxr.fr'; 
+    }, []); 
+    return null;
+}
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
-    <Canvas
-        camera={ {
-            fov: 45,
-            near: 0.1,
-            far: 2000,
-            position: [ 0, 0, 7 ]
-        } }
-    >
-        <Experience />
-    </Canvas>
+    <>
+        <MobileView>
+            <MobileRedirect />
+        </MobileView>
+        <BrowserView className="window_size">
+            <Canvas
+                camera={{
+                    fov: 45,
+                    near: 0.1,
+                    far: 2000,
+                    position: [0, 0, 7],
+                }}
+            >
+                <Experience />
+            </Canvas>
+        </BrowserView>
+    </>
 )
